3084eb112066d6d512a7a89a8dc08828918a0797
Efimov Kirill <eks1985@yandex.ru>
2021-09-07 17:44:49 +0300

    Remove unused code

--- store.ts
+++
@@ -1,9 +0,0 @@
-(function () {
-  console.log(window.location.pathname);
-  // console.log(window.state);
-  if (!window.state) {
-    window.state = 1;
-  } else {
-    window.state ++;
-  }
-})();
